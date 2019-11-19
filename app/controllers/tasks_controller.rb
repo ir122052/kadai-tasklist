bb69e951687d2a169fe6048b438ac6e6f8da3be9
ir.122052 <longhuiyantian@gmail.com>
2019-11-19 10:40:18 +0000

    for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,5 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in
-    before_action :set_task, only: [:show, :update, :edit, :destroy]
     before_action :correct_user, only: [:show, :edit, :update, :destroy]
     
     def index
@@ -49,9 +48,6 @@ class TasksController < ApplicationController
 
     private
 
-    def set_task
-        @task = Task.find(params[:id])
-    end
     # Strong Parameter
     def task_params
         params.require(:task).permit(:content, :status)
